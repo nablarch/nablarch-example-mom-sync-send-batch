7e833e60bda44d87682fd5768c4c64b90ad6c1ea
Taichi Uragami <backpaper0@gmail.com>
2018-02-27 10:15:01 +0900

    feat: nullの場合はnullを返すようになったのでnullチェックのif文は不要

--- src/main/java/com/nablarch/example/SendProjectInsertMessageAction.java
+++ src/main/java/com/nablarch/example/SendProjectInsertMessageAction.java
@@ -48,14 +48,10 @@ public class SendProjectInsertMessageAction extends BatchAction<SqlRow> {
         // 要求電文のデータレコードを用意するために、書式変換を行う。
         // (キーの大文字小文字変換はフレームワークが行うため実装不要
         Date projectStartDate = inputData.getDate("PROJECT_START_DATE");
-        if (projectStartDate != null) {
-            inputData.put("PROJECT_START_DATE", FormatterUtil.format("dateTime", projectStartDate, DATE_FORMAT));
-        }
+        inputData.put("PROJECT_START_DATE", FormatterUtil.format("dateTime", projectStartDate, DATE_FORMAT));
 
         Date projectEndDate = inputData.getDate("PROJECT_END_DATE");
-        if (projectEndDate != null) {
-            inputData.put("PROJECT_END_DATE", FormatterUtil.format("dateTime", projectEndDate, DATE_FORMAT));
-        }
+        inputData.put("PROJECT_END_DATE", FormatterUtil.format("dateTime", projectEndDate, DATE_FORMAT));
 
         // 要求電文を送信する。
         // 送信時に、フレームワークが「要求電文のフォーマット定義」に従って、dataRecord変数の内容を電文に変換する。
